9f5a8af0427f02081cf0482ce807491e6bcab71b
ZhouChenghan <501276687@qq.com>
2021-11-23 03:57:51 +0100

    env try

---
+++ lab0 env/build/CMakeFiles/MyDemo.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyDemo.dir/try.cpp.obj"
+  "CMakeFiles/MyDemo.dir/try.cpp.obj.d"
+  "MyDemo.exe"
+  "MyDemo.exe.manifest"
+  "MyDemo.pdb"
+  "libMyDemo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyDemo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
